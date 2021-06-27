b6a7aaa9a6b9f88af9a3aaa3e63e42ad5fdfbc04
ThasneemShehani <86557599+ThasneemShehani@users.noreply.github.com>
2021-06-27 18:00:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        A basic web dev React Js Bootcamp. Get taught by industry experts having
+        years of experience in their respective fields. And Having cracked
+        various top MNC's in the world. GET CERTIFIED by ShapeAI at the end of
+        the Bootcamp. And a chance to get certified by Microsoft Learn Student
+        Ambassadors (MLSA), Google Developers Group (GDG RANCHI) and Developers
+        Student Club (DSC RANCHI).
+      </p>
+    </div>
+  );
+}
+
+export default Info;
